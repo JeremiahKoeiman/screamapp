8a20544f870b78f2d5ff4d81fa3b5e3c7f8dadf0
Jeremiah Koeiman <jeremiah@e-heroes.nl>
2020-01-24 14:59:35 +0100

    still bussy

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -1,11 +1,9 @@
 import React from "react";
 import PropTypes from 'prop-types'
 import withStyles from "@material-ui/core/styles/withStyles";
-import theme from "../util/theme";
 
 import {connect} from "react-redux"
 import {editUserDetails} from "../redux/actions/userActions";
-import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
 
 
 const styles = (theme) => ({
@@ -41,4 +39,4 @@ const mapStateToProps = (state) => ({
     credentials: state.user.credentials
 })
 
-export default connect(mapStateToProps, {editUserDetails})(withStyles(styles)(EditDetails))
+export default connect(mapStateToProps, {editUserDetails})(withStyles(styles)(EditDetails));
