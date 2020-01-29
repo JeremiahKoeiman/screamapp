d4f552bc9b17073aea01951cce778ab11d2ddc74
JeremiahKoeiman <9001603@student.zadkine.nl>
2020-01-29 13:27:59 +0100

    jvbs

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, {Fragment} from "react";
 import PropTypes from 'prop-types'
 import withStyles from "@material-ui/core/styles/withStyles";
 
 import {connect} from "react-redux"
 import {editUserDetails} from "../redux/actions/userActions";
-
+import Tooltip from "@material-ui/core/Tooltip";
+import IconButton from "@material-ui/core/IconButton";
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
+import EditIcon from "@material-ui/icons/Edit";
 
 const styles = (theme) => ({
-    ...theme
+    ...theme.spreadThis
 })
 
 class EditDetails extends React.Component {
@@ -19,12 +28,40 @@ class EditDetails extends React.Component {
     }
 
     componentDidMount() {
-        const { credentials } = this.props
+        const {credentials} = this.props
+        this.mapUserDetailsToState(credentials)
+    }
+
+    mapUserDetailsToState = (credentials) => {
+        this.setState({
+            bio: credentials.bio ? credentials.bio : '',
+            website: credentials.website ? credentials.website : '',
+            location: credentials.location ? credentials.location : '',
+        })
+    }
+
+    handleOpen = () => {
+        this.setState({ open: true })
+        this.mapUserDetailsToState(this.props.credentials)
+    }
+
+    handleClose = () => {
+        this.setState({ open: false })
     }
 
     render() {
+        const {classes} = this.props
         return (
-            <div>dcj</div>
+            <Fragment>
+                <Tooltip title={"Edit details"} placement={"top"}>
+                    <IconButton onClick={this.handleOpen} className={classes.button}>
+                        <EditIcon color={"primary"}/>
+                    </IconButton>
+                </Tooltip>
+                <Dialog open={this.state.open} onClose={this.handleClose} fullWidth maxWidth={"sm"}>
+
+                </Dialog>
+            </Fragment>
         )
     }
 
